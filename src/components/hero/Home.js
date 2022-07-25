756c153527e3c15a6c0865a800c1de3124ad2969
Sam Petrosyan <81108029+Sam-tech94@users.noreply.github.com>
2022-07-25 21:22:45 +0400

    Update Home.js

--- src/components/hero/Home.js
+++ src/components/hero/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
        <div className="hero" id="home">
         <div className="content">
             <p>Call us</p>
-            <a href="tel:+374-77-77-77-77"><p>+374-77-77-77-77</p></a>
+            <a className="tel" href="tel:+374-77-77-77-77"><p>+374-77-77-77-77</p></a>
             <p>Because Money</p>
             <p>Doesn't come with instructions</p>
             <button className="button">Free Consultations</button>
